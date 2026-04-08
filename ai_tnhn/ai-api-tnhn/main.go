71c381454231d82bb74899536e8841c572031e62
Long Tran <reiway.vn@gmail.com>
2026-04-08 21:30:29 +0700

    [org] tra ve list theo role

--- ai_tnhn/ai-api-tnhn/main.go
+++ ai_tnhn/ai-api-tnhn/main.go
@@ -194,7 +194,7 @@ func main() {
 	contextWith := web.NewContextWith()
 
 	authHandler := handler.NewAuthHandler(authService, tokenService, contextWith, confg.OAuthConfig)
-	orgHandler := handler.NewOrganizationHandler(orgService, authService, contextWith)
+	orgHandler := handler.NewOrganizationHandler(orgService, authService, roleRepo, contextWith)
 	empHandler := handler.NewEmployeeHandler(empService, contextWith)
 	stationHandler := handler.NewStationHandler(stationService)
 	inuHandler := handler.NewInundationHandler(inuService, authService, contextWith)

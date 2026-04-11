f5079dcf40bcc49be55630431f38dc0be5472cee
Long Tran <reiway.vn@gmail.com>
2026-04-11 22:07:11 +0700

    [constant] xoa file thua

--- ai_tnhn/ai-api-tnhn/constant/session_feature.go
+++
@@ -1,7 +0,0 @@
-package constant
-
-const (
-	SessionFeatureTypeCustomerFeedback = "feedback"
-	SessionFeatureTypeCustomGame       = "custom_game"
-	SessionFeatureTypeLuckyDraw        = "lucky_draw"
-)
